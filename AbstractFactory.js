6c111acc9a5772f6ff7e5b6b3e15163f42f9c234
Gamerman2001 <Tro12185@gmail.com>
2020-10-26 00:52:51 -0400

    Factory Pattern set

---
+++ AbstractFactory.js
@@ -0,0 +1,65 @@
+// a Factory is an object that creates other objects
+const log = (function () {
+   let log = "";
+
+   return {
+      add: function (msg) {
+         log += msg + "\n";
+      },
+      show: function () {
+         console.log(log);
+         log = ''
+      },
+   };
+})();
+
+class Employee {
+   constructor(name) {
+      this.name = name;
+   }
+
+   say = () => {
+      log.add("I am employee " + this.name);
+   };
+}
+
+class EmployeeFactory {
+   create = (name) => {
+      return new Employee(name);
+   };
+}
+
+class Vendor {
+   constructor(name) {
+      this.name = name;
+
+      this.say = function () {
+         log.add("I am vendor " + name);
+      };
+   }
+}
+
+class VendorFactory {
+   create = (name) => {
+      return new Vendor(name);
+   };
+}
+
+const run = () => {
+   const persons = [];
+   const employeeFactory = new EmployeeFactory()
+   const vendorFactory = new VendorFactory()
+
+   persons.push(employeeFactory.create('Johnathan Gregg'))
+   persons.push(employeeFactory.create('Marcus Aurelius'))
+   persons.push(vendorFactory.create('Nike'))
+   persons.push(vendorFactory.create('Adidas'))
+
+   persons.map((name)=> {
+    name.say()
+   })
+
+   log.show()
+};
+
+run()
---
+++ patterns.md
@@ -0,0 +1,27 @@
+This repo will serve as a javascript implementation of design patterns. I'm writing these by hand so I can get exposed to what patterns may be used in different parts of app development. 
+
+# Patterns Covered:
+
+- Abstract Factory
+- Builder
+- Factory
+- Prototype
+- Singleton
+- Adapter
+- Bridge
+- Composite 
+- Decorator
+- Facade
+- Flyweight
+- Proxy
+- Chain of Resp.
+- Command
+- Interpreter
+- Iterator
+- Mediator
+- Memento
+- Observer
+- State
+- Strategy
+- Template Method
+- Visitor
