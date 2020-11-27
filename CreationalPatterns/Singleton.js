76a8194e0a6adeb9099a919cc962e07d8e1d18e8
Gamerman2001 <Tro12185@gmail.com>
2020-11-26 20:53:45 -0500

    Using a singleton Pattern added with example

--- CreationalPatterns/Singleton.js
+++ CreationalPatterns/Singleton.js
@@ -1,1 +1,34 @@
-//We use a singleton class 
+//We use a singleton class when we want to make a instance of something where only one type of it will exist. 
+
+const Singleton = function () {
+  let instance;
+
+  createInstance = () => {
+    const object = {string: 'I am in the instance'}
+    const object2 = new Object('testing how this looks as an instance')
+
+    return object2
+  }
+
+  return {
+    getInstance: function () {
+      if(!instance) {
+        instance = createInstance()
+      }
+
+      return instance
+    }
+  }
+}()
+
+function run() {
+  const instance1 = Singleton.getInstance()
+  const instance2 = Singleton.getInstance()
+
+  if(instance1 == instance2){
+    console.log(instance2)
+    console.log('these two instances are the same')
+  }
+}
+
+run()
